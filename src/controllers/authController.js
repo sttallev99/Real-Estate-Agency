442c056905c076225d43bee345eb39dda6e84756
Георги Сталев <sttallev99@gmail.com>
2023-02-01 23:24:39 +0200

    create Auth service and add body parser  into main index file

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 
+const authService = require('../services/authService.js')
+
 router.get('/register', (req, res) => {
     res.render('auth/register');
 });
+
+router.post('/register', (req, res) => {
+    console.log(req.body);
+    res.end();
+})
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
---
+++ src/services/authService.js
@@ -0,0 +1,10 @@
+const User = require('../models/User.js')
+
+
+exports.login = () => {
+
+}
+
+exports.register = (userData) => {
+    
+}
